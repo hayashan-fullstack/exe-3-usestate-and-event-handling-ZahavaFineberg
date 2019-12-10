39fb55243bffaa2d4c374158feefcae05edbe87e
ZahavaFineberg <57223829+ZahavaFineberg@users.noreply.github.com>
2019-12-10 21:19:14 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,75 @@
-import React, { useState,useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+// import { Timer } from "./components/timer";
+const WishList = [
 
-
-
-
-const App = (props) => {
-
-    const [s_counter, setCounter_s] = useState(0)
-    const [m_counter, setCounter_m] = useState(0)
-    const [h_counter, setCounter_h] = useState(0)
-    //console.log(counter);
-    const timeout=useRef(null)
-    timeout.current=setTimeout(()=>{
-        if (m_counter==59)
-        {
-            setCounter_m(0);
-            setCounter_h(h_counter+1);
-        }
-        if (s_counter==59)
-        {
-            setCounter_s(0);
-            setCounter_m(m_counter+1);
-        }
-        else setCounter_s(s_counter+1)
-    },1000)
-    const restart =() =>
     {
-        clearTimeout(timeout.current);
-        setCounter_h(0);
-        setCounter_s(0);
-        setCounter_m(0);
+        details: "DETAILS",
+        deadline: "DEADLINE",
+        importance: "IMPORTANCE",
+        done: "DONE??"
+    },
+    {
+        details: "wash dishes",
+        deadline: "tonight",
+        importance: "important",
+        done: "no"
+    },
+    {
+        details: "read c++",
+        deadline: "Tuesday",
+        importance: "urgent",
+        done: "a little"
+    },
+    {
+        details: "shopping",
+        deadline: "next year",
+        importance: "extremly important",
+        done: "I wish"
+    },
+    {
+        details: "go to sleep",
+        deadline: "RIGHT NOW",
+        importance: "needed",
+        done: "not at all"
+    },
+    {
+        details: "Buy Boots",
+        deadline: "THIS SECOND, I'M SOAKING",
+        importance: "SOS",
+        done: "not close"
+    },
+    {
+        details: "eat",
+        deadline: "when hungry",
+        importance: "a little",
+        done: "chalomot balaila"
     }
-    return (
-        <div style={{ margin: '10%' }}>
-            <h1> timer</h1>
-            <h2> {h_counter}:{m_counter}:{s_counter}</h2>
-            <br />
-            { <button onClick={restart}>restart</button> }
-        </div>
+ 
+ 
+];
+
+  const OutputScreenRow = (props) => { 
+    return ( 
+        
+      <div className="screen-row" style={{display:'inline'}}> 
+        <input type="text" readOnly value={props.value} style={{color: 'red'}} /> 
+      </div> 
+    ) 
+  }   
+
+const Line=(props)=>{
+    return(
+        <div >< OutputScreenRow value={props.line.details}/>< OutputScreenRow value={props.line.deadline}/>< OutputScreenRow value={props.line.
+            importance}/>< OutputScreenRow value={props.line.done}/> </div>
     )
- }
+}
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const App = () => {
+    return (
+        <div  style={{color:"Green"}}>
+        <h2 style={{backgroundColor: "lightblue"}}>My Wish list</h2>
+         { WishList.map((line,i)=> <Line line={WishList[i]} key={i}/> )}</div>
+    );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
